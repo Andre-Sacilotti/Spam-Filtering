c5767a2c2cb20d1d8b1e059bdc833c3974d698c8
André Sacilotti <56316646+Andre-Sacilotti@users.noreply.github.com>
2020-05-05 02:57:00 -0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"andre.sacilotti@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
